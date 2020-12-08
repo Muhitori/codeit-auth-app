8df83bf051559cf078181ea258bf06ed7c867a32
Muhitori <39372404+Muhitori@users.noreply.github.com>
2020-12-08 16:52:38 +0200

    .

--- server/connection/connection.ts
+++ server/connection/connection.ts
@@ -1,24 +1,22 @@
 import { createConnection} from "typeorm";
-import  path  from 'path';
+import path from 'path';
+import fs from 'fs';
 import { Country } from '../entities/Country';
 import { User } from '../entities/User';
   
 async function makeConnection() {
-  const connection = await createConnection({
-		username: process.env.POSTGRES_USER,
-		password: process.env.POSTGRES_PASSWORD,
-		database: process.env.POSTGRES_DATABASE,
-		host: process.env.POSTGRES_HOST,
+	const connection = await createConnection({
+		username: process.env.POSTGRES_USER || "postgres",
+		password: process.env.POSTGRES_PASSWORD || "postgres",
+		database: process.env.POSTGRES_DATABASE || "userDB",
+		host: process.env.POSTGRES_HOST || "localhost",
 		port: 5432,
 		type: "postgres",
 		synchronize: false,
 		entities: [Country, User],
 		migrations: [path.resolve(__dirname, "..", "migrations", "**")],
-		ssl: true,
-		extra: {
-			ssl: {
-				rejectUnauthorized: false,
-			},
+		ssl: {
+			rejectUnauthorized: false,
 		},
 	});
 	console.log(connection);
