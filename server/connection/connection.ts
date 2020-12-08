d16e67f3313307559be5189d7271c5d5a06b6b81
Muhitori <39372404+Muhitori@users.noreply.github.com>
2020-12-08 16:10:05 +0200

    process env

--- server/connection/connection.ts
+++ server/connection/connection.ts
@@ -6,10 +6,10 @@ import { User } from '../entities/User';
 async function makeConnection() {
   const entitiesPath = path.resolve(__dirname, "..", "entities", "./**");
   const connection = await createConnection({
-		username: "postgres" || process.env.POSTGRES_USER,
-		password: "postgres" || process.env.POSTGRES_PASSWORD,
-		database: "userDB" || process.env.POSTGRES_DATABASE,
-		host: "127.0.0.1" || process.env.POSTGRES_HOST,
+		username: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PASSWORD,
+		database: process.env.POSTGRES_DATABASE,
+		host: process.env.POSTGRES_HOST,
 		type: "postgres",
 		synchronize: false,
 		entities: [Country, User],
