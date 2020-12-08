b12cec41de8a1dcc7143ff88ae9f4c8e89fe6c44
Muhitori <39372404+Muhitori@users.noreply.github.com>
2020-12-08 21:04:16 +0200

    ...

--- server/scripts/migration.ts
+++ server/scripts/migration.ts
@@ -1,20 +1,29 @@
 import path from 'path';
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import envConfig from "../config/config";
 import { Country } from '../entities/Country';
 import { User } from '../entities/User';
 
 async function bootstrap() {
 	try {
 		const connection = await createConnection({
-			...envConfig,
+			username: "deptpxctnrksxu",
+			password:
+				"d3e43d392ea28fae6f1d692c3adca8be055c77e7e567a249ddbe8ecc51c23c00",
+			database: "d8eikoefbc936b",
+			host: "ec2-54-75-248-49.eu-west-1.compute.amazonaws.com",
 			port: 5432,
 			type: "postgres",
 			synchronize: false,
 			logging: true,
 			entities: [Country, User],
 			migrations: [path.resolve(__dirname, "..", "migrations", "**")],
+			ssl: true,
+			extra: {
+				ssl: {
+					rejectUnauthorized: false,
+				},
+			},
 		});
 		console.log("============== db connected ==============");
 		const migrations = await connection.runMigrations();
