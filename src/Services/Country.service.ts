d5f44cbb1dc35836505ed75e81d12dac2beb0afb
Muhitori <39372404+Muhitori@users.noreply.github.com>
2020-12-08 22:38:14 +0200

    s

--- src/Services/Country.service.ts
+++ src/Services/Country.service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const URL =
 	process.env.NODE_ENV === "production"
-		? "http://ec2-54-75-248-49.eu-west-1.compute.amazonaws.com:9000/"
+		? "https://ec2-54-75-248-49.eu-west-1.compute.amazonaws.com:9000/"
 		: "http://localhost:9000/";
 
 export class CountryService {
