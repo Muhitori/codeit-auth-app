821621d7f8c989f98e0d786a1dddbf1d33652744
Muhitori <39372404+Muhitori@users.noreply.github.com>
2020-12-08 22:10:01 +0200

    >_>

--- server/config/config.js
+++ server/config/config.js
@@ -1,22 +1,23 @@
 const envConfig =
 	process.env.NODE_ENV === "production"
 		? {
-				username: process.env.POSTGRES_USER,
-				password: process.env.POSTGRES_PASSWORD,
-				database: process.env.POSTGRES_DATABASE,
-				host: process.env.POSTGRES_HOST,
+				username: "deptpxctnrksxu",
+				password:
+					"d3e43d392ea28fae6f1d692c3adca8be055c77e7e567a249ddbe8ecc51c23c00",
+				database: "d8eikoefbc936b",
+				host: "ec2-54-75-248-49.eu-west-1.compute.amazonaws.com",
 				ssl: true,
 				extra: {
 					ssl: {
 						rejectUnauthorized: false,
 					},
 				},
-		}
+		  }
 		: {
 				username: "postgres",
 				password: "postgres",
 				database: "userDB",
 				host: "127.0.0.1",
-		};
+		  };
 
 module.exports = envConfig;
--- src/Services/Country.service.ts
+++ src/Services/Country.service.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const URL = "http://localhost:9000/";
+const URL =
+	process.env.NODE_ENV === "production"
+		? "ec2-54-75-248-49.eu-west-1.compute.amazonaws.com:9000"
+		: "http://localhost:9000/";
 
 export class CountryService {
 	getCountries() {
